b272bb830f99ccd28d892ad65325f8cc17fa8d7f
Frdownnn <70484284+arifDev21@users.noreply.github.com>
2024-09-11 09:42:40 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 export const api = axios.create({
   // baseURL: 'http://localhost:9000',
   // baseURL: 'https://spiky-brassy-snowdrop.glitch.me/',
-  baseURL: 'https://rigorous-charm-nurse.glitch.me/',
+  baseURL: 'https://puddle-stellar-albacore.glitch.me/',
 });
